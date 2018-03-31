0e5cf3284204da78760d8f59812ecd41f77cc1e3
Abdurrahman Shaleh <shaleh2105@gmail.com>
2018-03-31 11:01:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Olah Nilai</title>
-
-</head>
-<body>
-	<div>
-		<h1>Olah Nilai</h1>
-		<h2>Masukan Nilai</h2>
-		<form method="POST" action="tampil.php">
-			<label>UTS   : </label><input type="number" name="uts" value="" required="" min="0" max="100"><br>
-			<label>UAS   : </label><input type="number" name="uas" value="" required="" min="0" max="100"><br>
-			<label>Quiz  : </label><input type="number" name="quiz" value="" required="" min="0" max="100"><br>
-			<label>Tugas : </label><input type="number" name="tugas" value="" required="" min="0" max="100"><br>
-			<input type="submit" name="submit" value="Proses">
-		</form>
-	</div>
-</body>
-</html>
+<?php
+	echo "Hello ";
+?>
